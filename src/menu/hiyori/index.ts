22b5b51a1beb3b253a4a853857fa8ec4677dd9ec
saltcute <me@mizuno.ai>
2024-07-09 14:57:34 -0400

    chore: update dependencies

--- src/menu/hiyori/index.ts
+++ src/menu/hiyori/index.ts
@@ -13,17 +13,6 @@ export default menu;
 client.plugin.load(menu);
 client.plugin.addAlias(menu, "hyr", "hi", "h");
 
-const basicPath = upath.join(__dirname, "command");
-const commands = fs.readdirSync(basicPath);
-for (const command of commands) {
-    try {
-        require(upath.join(basicPath, command));
-    } catch (e) {
-        menu.logger.error("Error loading command");
-        menu.logger.error(e);
-    }
-}
-
 import WordleMenu from "@saltcute/kasumi-wordle";
 import EssentialMenu from "@saltcute/kasumi-essential";
 import ApexMenu from "@saltcute/kasumi-apex";
@@ -31,3 +20,16 @@ import ApexMenu from "@saltcute/kasumi-apex";
 menu.load(new EssentialMenu());
 menu.load(new WordleMenu());
 menu.load(new ApexMenu());
+
+try {
+    const basicPath = upath.join(__dirname, "command");
+    const commands = fs.readdirSync(basicPath);
+    for (const command of commands) {
+        try {
+            require(upath.join(basicPath, command));
+        } catch (e) {
+            menu.logger.error("Error loading command");
+            menu.logger.error(e);
+        }
+    }
+} catch (e) {}
