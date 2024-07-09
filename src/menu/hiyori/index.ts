8d4498918a38a27a0c4e2db15777dfd43a325d9b
saltcute <me@mizuno.ai>
2024-07-09 07:45:34 -0400

    feat: add wordle and apex modules

---
+++ src/menu/hiyori/index.ts
@@ -0,0 +1,33 @@
+import { client } from "init/client"
+import { BaseMenu } from "kasumi.js"
+import upath from "upath"
+import * as fs from "fs"
+import "./event"
+
+class HiyoriMenu extends BaseMenu {
+    name = "hiyori"
+}
+
+const menu = new HiyoriMenu()
+export default menu
+client.plugin.load(menu)
+client.plugin.addAlias(menu, "hyr", "hi", "h")
+
+const basicPath = upath.join(__dirname, "command")
+const commands = fs.readdirSync(basicPath)
+for (const command of commands) {
+    try {
+        require(upath.join(basicPath, command))
+    } catch (e) {
+        menu.logger.error("Error loading command")
+        menu.logger.error(e)
+    }
+}
+
+import WordleMenu from "@saltcute/kasumi-wordle"
+import EssentialMenu from "@saltcute/kasumi-essential"
+import ApexMenu from "@saltcute/kasumi-apex"
+
+menu.load(new EssentialMenu())
+menu.load(new WordleMenu())
+menu.load(new ApexMenu())
